ac3211d3a3ad8060ba99681bdd88e9ca6176a6e2
RubenS98 <A01021759@itesm.mx>
2017-11-12 16:53:43 -0600

    Fibonacci

--- Intento1.java
+++ Intento1.java
@@ -1,1 +1,22 @@
-Hola
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n;
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Dame el numero");
+        n=lectura.nextInt;
+        
+        for (int i = 1; i <= n; i++){
+            System.out.println(i + ": " + fibonacci(i));
+        }
+    }
+    public static long fibonacci(int n) {
+        if (n <= 1){
+          return n;
+        }
+        else{
+          return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
